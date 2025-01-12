14545304071fac9b0ed590a2240a4dfb7d2fc8f4
Firdaus Adi Putra <mail@firdaus.org>
2025-01-12 17:17:09 +0700

    Add a seeder

---
+++ database/seeders/20250112091623-insert-one-live-record.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'lives',
+      [
+        {
+          ku1live: 'https://www.youtube.com/watch?v=XqZsoesa55w',
+          ku2live: 'https://www.youtube.com/watch?v=XqZsoesa55w',
+          ku5live: 'https://www.youtube.com/watch?v=XqZsoesa55w',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        // Add more rows as needed
+      ],
+      {},
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // await queryInterface.bulkDelete('lives', null, {});
+  },
+};
